ccc8678e10d0b3e988a457e1a3009046093413dc
Ndabezinhletsh <ndabezinhletsh@gmail.com>
2022-09-08 12:40:22 -0700

    printf correct

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Description : prints a statement using printf
  * Returnt : Always 0 (Success)
  */
-int main()
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
