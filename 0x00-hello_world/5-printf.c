d0da361edfac910afda554e33de7b6994875df3a
Ndabezinhletsh <ndabezinhletsh@gmail.com>
2022-09-08 12:32:24 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main : entry point
+ * Description : prints a statement using printf
+ * Returnt : 0 (Always)
+ */
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+} 
