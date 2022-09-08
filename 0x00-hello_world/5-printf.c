7732aa3554227dba9e32cf32d5d71efc26b85d44
Ndabezinhletsh <ndabezinhletsh@gmail.com>
2022-09-08 13:13:41 -0700

    printf correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main : this is were the program runs
- * Description : prints a statement using printf
- * Returnt : Always 0 (Success)
+ *main - this is where program execution occurs
+ *Description: the program prints a statement using printf
+ * the statement is "with proper grammar,
+ * but the outcome is a piece of art,"
+ *Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);}
+printf("with proper grammar, but the outcome is a piece of art,");
+ return (0);
+} 
