e43329e09966eacd079107dabe9e45354caa5950
Ndabezinhletsh <ndabezinhletsh@gmail.com>
2022-09-12 03:31:28 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Description: using the main function
+ * this program prints sizes of different data types
+ * Return: 0
+ */
+int main(void)
+{
+char charType;
+int intType;
+long int intlongType;
+long int intlonglongType;
+float floatType;
+printf("Size of char: %zu byte (s)\n", sizeof(charType));
+printf("Size of int: %zu byte (s)\n", sizeof(intType));
+printf("Size of long int: %zu byte (s)\n", sizeof(intlongType));
+printf("Size of long long int: %zu byte (s)\n", sizeof(intlonglongType));
+printf("Size of float: %zu byte (s)\n", sizeof(floatType));
+return (0);
+}
