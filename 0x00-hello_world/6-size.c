c32e20995c997bfd80c8067bfca7ce48333dc6b1
Ndabezinhletsh <ndabezinhletsh@gmail.com>
2022-09-12 03:38:07 -0700

    correct

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ float floatType;
 printf("Size of char: %zu byte(s)\n", sizeof(charType));
 printf("Size of int: %zu byte(s)\n", sizeof(intType));
 printf("Size of long int: %zu byte(s)\n", sizeof(intlongType));
-printf("Size of long long int: %zu byte (s)\n", sizeof(intlonglongType));
+printf("Size of long long int: %zu byte(s)\n", sizeof(intlonglongType));
 printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
