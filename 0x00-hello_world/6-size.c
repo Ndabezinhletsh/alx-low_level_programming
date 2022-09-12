5960f0c889fc51de0ea0e1b04a1108b0e3598e68
Ndabezinhletsh <ndabezinhletsh@gmail.com>
2022-09-12 03:56:03 -0700

    correct

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int intType;
 long int intlongType;
 long long int intlonglongType;
 float floatType;
-printf("Size of a char: %ld byte(s) \n", sizeof(charType));
-printf("Size of an int: %ld byte(s) \n", sizeof(intType));
-printf("Size of a long int: %ld byte(s) \n", sizeof(intlongType));
-printf("Size of a long long int: %ld byte(s) \n", sizeof(intlonglongType));
-printf("Size of a float: %ld byte(s) \n", sizeof(floatType));
+printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+printf("Size of an int: %ld byte(s)\n", sizeof(intType));
+printf("Size of a long int: %ld byte(s)\n", sizeof(intlongType));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(intlonglongType));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 return (0);
 }
