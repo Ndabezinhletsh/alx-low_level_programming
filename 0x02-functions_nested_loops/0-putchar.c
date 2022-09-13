1fa02b09bfbe72a4c8782380161333bd39a040da
Ndabezinhletsh <ndabezinhletsh@gmail.com>
2022-09-13 00:02:41 -0700

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: using the main function
+ * this program prints "_putchar on the screen"
+ * Return: 0
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
